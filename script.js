2f549867483d35b3825b82fa9009a5db238b5c6f
Yaowwwwww <yaoww@berkeley.edu>
2025-10-14 20:06:42 -0700

    proj1

---
+++ script.js
@@ -0,0 +1,7 @@
+// Theme Toggle Function
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+});
+
